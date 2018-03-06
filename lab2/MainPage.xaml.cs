fcd458a632e4f94dd42c0b5db3fc9e6c4999cd5c
RomanYazvinsky <32144682+RomanYazvinsky@users.noreply.github.com>
2018-03-06 09:48:07 +0300

    Peterson's alg. Changing alg with button

--- lab2/MainPage.xaml.cs
+++ lab2/MainPage.xaml.cs
@@ -19,51 +19,83 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace lab2
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-        private LinkedList<string> logList = new LinkedList<string>();
-        private volatile bool thread1IsWorking = true;
-        private volatile bool thread2IsWorking = false;
-        private volatile bool textEnterred = false;
-        private bool thread1Execution = true;
-        private bool thread2Execution = true;
-        private Thread thread1;
-        private Thread thread2;
+        private int _algorithm = 0;
+        private int _true = 1;
+        private int _false = 0;
+        private int _n = 2;
+        private int _turn = 0;
+        private int[] _interested = new int[2];
+        private LinkedList<string> _logList = new LinkedList<string>();
+        private volatile bool _thread1IsWorking = true;
+        private volatile bool _thread2IsWorking = false;
+        private volatile bool _textEnterred = false;
+        private bool _thread1Execution = true;
+        private bool _thread2Execution = true;
+        private Thread _thread1;
+        private Thread _thread2;
+        private Thread _thread1Backup;
+        private Thread _thread2Backup;
         private int i = 0;
 
         public MainPage()
         {
             this.InitializeComponent();
-            thread1 = new Thread(this.threadMethod1);
-            thread1.Start();
-            thread2 = new Thread(this.threadMethod2);
-            thread2.Start();
+            _thread1 = new Thread(this.ThreadPeterson1);
+            _thread1.Priority = ThreadPriority.Lowest;
+            _thread1.Start();
+            _thread2 = new Thread(this.ThreadPeterson2);
+            _thread2.Priority = ThreadPriority.Lowest;
+            _thread2.Start();
+            _thread1Backup = new Thread(this.ThreadAlternation1);
+            _thread1Backup.Priority = ThreadPriority.Lowest;
+            _thread1Backup.Start();
+            _thread2Backup = new Thread(this.ThreadAlternation2);
+            _thread2Backup.Priority = ThreadPriority.Lowest;
+            _thread2Backup.Start();
+            
         }
 
-        private void threadMethod1()
+        private void ThreadPeterson1()
         {
-            log("Thread #1 started");
-            while (thread1Execution)
+            AddLog("Thread #1 is ready. Algorithm: Peterson's");
+            while (_thread1Execution)
             {
-                while (thread2IsWorking) ;
-                thread1IsWorking = true;
-                criticalRegion1();
-                thread1IsWorking = false;
-                log("Thread1 critical region is over");
+                while (_algorithm == 1) ;
+                EnterRegion(0);
+                CriticalRegion1();
+                LeaveRegion(0);
+           //     AddLog("Thread1 critical region is over");
+            }
+          
+        }
+
+        private void ThreadAlternation1()
+        {
+            AddLog("Thread #1 is ready. Algorithm: alternation");
+            while (_thread1Execution)
+            {
+                while (_algorithm == 0) ;
+                while (_thread2IsWorking) ;
+                _thread1IsWorking = true;
+                CriticalRegion1();
+                _thread1IsWorking = false;
+           //     AddLog("Thread1 critical region is over");
             }
         }
 
-        private void criticalRegion1()
+        private void CriticalRegion1()
         {
-            log("Thread1 is waiting for input");
-            while (!textEnterred) ;
+            AddLog("Thread1 is waiting for input... Alg:"+_algorithm);
+            int k = _algorithm;
+            while (!_textEnterred);
+            if (k != _algorithm)
+            {
+                return;
+            }
             string input = GetText().Result;
             StringBuilder result = new StringBuilder();
             foreach (var symb in input)
@@ -71,28 +103,48 @@ namespace lab2
                 result.Append((int) symb);
             }
             SetText(result.ToString());
-            textEnterred = false;
+            _textEnterred = false;
+        }
+
+        private void ThreadPeterson2()
+        {
+            AddLog("Thread #2 is ready. Algorithm: Peterson's");
+            while (_thread2Execution)
+            {
+                while (_algorithm == 1) ;
+                EnterRegion(1);
+                CriticalRegion2();
+                LeaveRegion(1);
+               // AddLog("Thread2 critical region is over");
+            }
+
         }
 
 
-        private void threadMethod2()
+        private void ThreadAlternation2()
         {
-            log("Thread #2 started");
-            while (thread2Execution)
+            AddLog("Thread #2 is ready. Algorithm: alternation");
+            while (_thread2Execution)
             {
-                while (thread1IsWorking) ;
-                thread2IsWorking = true;
-                criticalRegion2();
-                thread2IsWorking = false;
-                log("Thread2 critical region is over");
+                while (_algorithm == 0) ;
+                while (_thread1IsWorking) ;
+                _thread2IsWorking = true;
+                CriticalRegion2();
+                _thread2IsWorking = false;
+              //  AddLog("Thread2 critical region is over");
             }
         }
 
 
-        private void criticalRegion2()
+        private void CriticalRegion2()
         {
-            log("Thread2 is waiting for input");
-            while (!textEnterred) ;
+            AddLog("Thread2 is waiting for input... Alg:" + _algorithm);
+            int k = _algorithm;
+            while (!_textEnterred);
+            if (k != _algorithm)
+            {
+                return;
+            }
             string input = GetText().Result;
             StringBuilder result = new StringBuilder();
             foreach (var symb in input)
@@ -107,33 +159,48 @@ namespace lab2
                 }
             }
             SetText(result.ToString());
-            textEnterred = false;
+            _textEnterred = false;
+        }
+
+
+        private void EnterRegion(int process) // номер процесса - 0 или 1
+        {
+            int other;
+            other = 1 - process;    // точка 1
+            _interested[process] = _true;  // точка 2 (заинтересованный процесс)
+            _turn = process;         // точка 3
+            while (_turn == process && _interested[other] == _true) ; // активное ожидание
+        }
+
+        private void LeaveRegion(int process) // номер процесса - 0 или 1
+        {
+            _interested[process] = 0; // признак выхода из критической секции
         }
 
         private async Task<string> GetText()
         {
             string s = null;
-            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => { s = Proc1.Text; });
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => { s = InputField.Text; });
             return s;
         }
 
         private async Task SetText(string value)
         {
-            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => { Proc1.Text = value; });
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => { InputField.Text = value; });
         }
 
-        private async void log(string text)
+        private async void AddLog(string text)
         {
             await this.Dispatcher.RunAsync(CoreDispatcherPriority.Low, () =>
             {
-                logList.AddFirst(LastLog.Text);
+                _logList.AddFirst(LastLog.Text);
                 LastLog.Text = ++i + ") " + text;
-                if (logList.Count > 12)
+                if (_logList.Count > 12)
                 {
-                    logList.RemoveLast();
+                    _logList.RemoveLast();
                 }
                 StringBuilder logText = new StringBuilder();
-                foreach (var logNode in logList)
+                foreach (var logNode in _logList)
                 {
                     logText.Append(logNode).Append(Environment.NewLine);
                 }
@@ -143,7 +210,26 @@ namespace lab2
 
         private void Enter_Click(object sender, RoutedEventArgs e)
         {
-            textEnterred = true;
+            _textEnterred = true;
+        }
+
+        private void Alternate_Click(object sender, RoutedEventArgs e)
+        {
+            if (_algorithm == 0)
+            {
+                _algorithm = 1;
+            }
+            else
+            {
+                _algorithm = 0;
+            }
+            Thread t;
+            t = _thread1Backup;
+            _thread1Backup = _thread1;
+            _thread1 = t;
+            t = _thread2Backup;
+            _thread2Backup = _thread2;
+            _thread2 = t;
         }
     }
 }
